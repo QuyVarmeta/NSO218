f499b396b1d73b96f9b4639c733558c0324b3a74
Quy le <Quybt93@gmail.com>
2025-02-05 14:13:19 +0700

    Init

---
+++ Assets/Scripts/Assembly-CSharp/TouchScreenKeyboard.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+public class TouchScreenKeyboard
+{
+	public static bool hideInput;
+
+	public static bool visible;
+
+	public bool done;
+
+	public string text;
+
+	public static UnityEngine.TouchScreenKeyboard Open(string text, TouchScreenKeyboardType t, bool b1, bool b2, bool type, bool b3, string caption)
+	{
+		if (Application.platform is RuntimePlatform.IPhonePlayer || Application.platform is RuntimePlatform.Android)
+        {
+			hideInput = true;
+			return UnityEngine.TouchScreenKeyboard.Open(text, (UnityEngine.TouchScreenKeyboardType)(int)t, b1, b2, type, b3, caption);
+        }
+		return null;
+	}
+
+	public static void Clear()
+	{
+		visible = false;
+	}
+}
